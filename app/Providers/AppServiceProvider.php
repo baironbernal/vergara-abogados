c7ba5d0e8eb1747e1b3ebfe5684af00181586f0a
Bairon Bernal <33238184+baironbernal@users.noreply.github.com>
2025-09-02 14:22:48 -0500

    update of this

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+         // Dynamically set public path depending on environment
+         $this->app->bind('path.public', function () {
+            // Check if "public_html" exists one level above base_path
+            $publicPath = base_path('../public_html');
+
+            if (is_dir($publicPath)) {
+                return $publicPath; // Use Hostinger public_html
+            }
+
+            return base_path('public'); // Default local dev
+        });
     }
 
     /**
